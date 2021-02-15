26fa49c16d2d49824a81492baf746bd6f8b127cf
chald80 <72771464+chald80@users.noreply.github.com>
2021-02-15 14:06:37 +0100

    update

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyQueue
+{
+    class MyQueue
+    {
+        public int[] queue;
+        private int top = 0;
+        private int _size;
+        public MyQueue(int size)
+        {
+            queue = new int[size];
+            _size = size;
+        }
+        public void Enqueue(int element)
+        {
+            
+        }
+        // public Dequeue()
+        //{
+
+        //}
+
+
+
+    }
+}
---
+++ MyQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue Q = new MyQueue(5);
+
+            Q.Enqueue(1);
+            Q.Enqueue(2);
+            Q.Enqueue(3);
+
+            foreach(var i in Q)
+            {
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
